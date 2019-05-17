2483e91fc39123ed425ee235b82f72e9d9c56f2d
JonesfromHan <jonas-boeckmann@gmx.de>
2019-05-17 09:13:34 +0200

    Changed german week and month abbreviation

--- src/locale/de.js
+++ src/locale/de.js
@@ -3,8 +3,9 @@ import dayjs from 'dayjs'
 const locale = {
   name: 'de',
   weekdays: 'Sonntag_Montag_Dienstag_Mittwoch_Donnerstag_Freitag_Samstag'.split('_'),
+  weekdaysMin: 'So._Mo._Di._Mi._Do._Fr._Sa.'.split('_'),
   months: 'Januar_Februar_März_April_Mai_Juni_Juli_August_September_Oktober_November_Dezember'.split('_'),
-  monthsShort: 'Jan_Feb_März_Apr_Mai_Juni_Juli_Aug_Sept_Okt_Nov_Dez'.split('_'),
+  monthsShort: 'Jan._Feb._März_Apr._Mai_Juni_Juli_Aug._Sep._Okt._Nov._Dez.'.split('_'),
   ordinal: n => `${n}.`,
   weekStart: 1,
   formats: {
